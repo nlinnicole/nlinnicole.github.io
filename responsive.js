7d2e505d98bffa531886c861e5d3c8078ff3e13d
Nicole Lin <nlin.nicole@gmail.com>
2019-03-22 22:01:43 -0400

    close button actually closes

--- responsive.js
+++ responsive.js
@@ -9,7 +9,22 @@ function openNav() {
     var close = document.getElementById("links-close");
     if (close.className === "linkclose") {
         close.className += " responsive-close";
-        console.log("hello");
+    } else {
+        close.className = "linkclose";
+    }
+}
+
+function closeNav() {
+    var overlay = document.getElementById("links-wrapper");
+    if (overlay.className === "linkwrap") {
+        overlay.className -= " responsive-overlay";
+    } else {
+        overlay.className = "linkwrap";
+    }
+
+    var close = document.getElementById("links-close");
+    if (close.className === "linkclose") {
+        close.className -= " responsive-close";
     } else {
         close.className = "linkclose";
     }
