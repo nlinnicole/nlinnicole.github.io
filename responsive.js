92324355fbf009e5398198221ce4a28316d15c0b
Nicole Lin <nlin.nicole@gmail.com>
2019-03-22 22:00:19 -0400

    close button working on responsive overlay

--- responsive.js
+++ responsive.js
@@ -1,8 +1,16 @@
-function changeToHamburger() {
-    var x = document.getElementById("links-wrapper");
-    if (x.className === "linkwrap") {
-        x.className += " responsive-overlay";
+function openNav() {
+    var overlay = document.getElementById("links-wrapper");
+    if (overlay.className === "linkwrap") {
+        overlay.className += " responsive-overlay";
     } else {
-        x.className = "linkwrap";
+        overlay.className = "linkwrap";
+    }
+
+    var close = document.getElementById("links-close");
+    if (close.className === "linkclose") {
+        close.className += " responsive-close";
+        console.log("hello");
+    } else {
+        close.className = "linkclose";
     }
 }
